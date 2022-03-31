de9755792000c98f54cc38cb029c2c7cf7fa142a
Simara08 <7knywpc@code.edu.az>
2022-03-31 08:55:33 +0400

    ac.modiencap

---
+++ Projestudent/Projestudent/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Projestudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Student student1 = new Student(77, "Gulchin", "Taghizade", "CS");
+
+            Group gr1 = new Group();
+            gr1.AddStudent(student1);
+        }
+    }
+}
