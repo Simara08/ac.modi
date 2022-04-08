6326b24a3d2c96d58faad7f82c273cecc55fa86a
Simara08 <100687456+Simara08@users.noreply.github.com>
2022-04-08 18:54:39 +0400

    Add files via upload

---
+++ customdelegate/customdelegate/ICompare.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace customdelegate
+{
+    interface Interface1
+    {
+    }
+}
---
+++ customdelegate/customdelegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace customdelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("elehber","Maharramli",21);
+            Person person2 = new Person("Simara","Maharramli",20);
+            Person person3 = new Person("Simara","Maharramli",20);
+            Person person4 = new Person("Eli","Maharramli",21);
+           
+
+            List<Person> persons = new List<Person>();
+            persons.Add(person1);
+            persons.Add(person2);
+            persons.Add(person3);
+            persons.Add(person4);
+            //persons.Find(x => x.Age == 21);
+            //Console.WriteLine(persons.Find(Person => Person.Age == 21).Name);
+            //foreach (var item in persons.FindAll(Person => Person.Age == 21))
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+            Console.WriteLine(persons.FindLast(x => x.Age == 21).Name);
+            Console.WriteLine(persons.Exists(x => x.Name == "bdhshbds"));
+
+            persons.Sort((a,b)=>a.Age-b.Age);
+            foreach (Person item in persons)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+    }
+}
